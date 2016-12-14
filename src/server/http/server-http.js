50b11b402a75dfa17caf65bd550a2bc2f29217c5
david lee <david@leedium.com>
2016-12-14 09:45:14 -0500

    adding socketcluster to build

--- src/server/http/server-http.js
+++ src/server/http/server-http.js
@@ -7,19 +7,29 @@ import express from 'express';
 import httpProxy from 'http-proxy';
 import bodyparser from 'body-parser';
 
-export const RESTServer = () =>
+let proxy = null;
+const ssl = {
+  key: fs.readFileSync(path.join(__dirname,'../../../', process.env.SSL_KEY)),
+  cert: fs.readFileSync(path.join(__dirname,'../../../', process.env.SSL_CERT,)),
+};
+if (process.env.NODE_ENV === 'development') {
+  proxy = httpProxy.createServer({
+    changeOrigin: true,
+    ws: true,
+    ssl,
+  });
+}
+
+const RESTServer = () =>
   new Promise((resolve, reject) => {
     const app = express();
     const port = process.env.REST_PORT || 3001;
-    const ssl = {
-      key: fs.readFileSync(path.resolve(__dirname, process.env.SSL_KEY)),
-      cert: fs.readFileSync(path.resolve(__dirname, process.env.SSL_CERT)),
-    };
+
 
     app.use(bodyparser.json());
 
     if (process.env.NODE_ENV === 'development') {
-      const proxy = httpProxy.createServer({
+      proxy = httpProxy.createServer({
         changeOrigin: true,
         ws: true,
         ssl,
@@ -43,4 +53,5 @@ export const RESTServer = () =>
       });
   });
 
+export { proxy };
 export default RESTServer;
