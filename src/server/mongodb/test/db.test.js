4affdabd495559c9b144f742376470c30e01a5b8
david lee <david@leedium.com>
2016-12-17 13:17:44 -0500

    db tests cleanup

--- src/server/mongodb/test/db.test.js
+++ src/server/mongodb/test/db.test.js
@@ -1,26 +1,20 @@
 import expect from 'expect';
-import jwt from 'jsonwebtoken';
 import { ObjectID } from 'mongodb';
 
-
-import {createPost, createUser} from '../model/helpers';
+import { createPost, createUser } from '../model/helpers';
 import mongodbConnect from '../mongodb-connect';
 import User from '../model/User';
 import Post from '../model/Post';
 import UserType from '../../../api/types/UserType';
 
 let mongoose;
-
 const user1ID = new ObjectID();
 const user2ID = new ObjectID();
 const user3ID = new ObjectID();
-
 const postID = new ObjectID();
 
 //  lifecycle methods
 before((done) => {
-
-
   mongodbConnect().then((db) => {
     const users = [
       createUser(user2ID, UserType.business, 'poo@poo.com', '362y32hjh3', 'leedium22', 'Ruth'),
@@ -30,7 +24,7 @@ before((done) => {
     mongoose = db;
     Post.remove({}).then(() => {
       User.remove({}).then(() => {
-        User.insertMany(users).then(() => done(), err => console.log(err));
+        User.insertMany(users).then(() => done(), err => err);
       });
     });
   });
@@ -42,16 +36,18 @@ after((done) => {
 });
 //  end lifecycle;
 
-
 describe('MongoDB User CRUD', () => {
-  it('Should SAVE a User', () => {
+  it('Should SAVE a User with JWT Token', () => {
     const newUser = createUser(user1ID);
     return newUser.save().then((user) => {
-      return user.toJSON();
-
       expect(user.tokens.length > 0).toBe(true);
       expect(user.email).toBe(newUser.email);
+      return user.toJSON();
     });
+  });
+
+  it('Should VALIDATE User by Token', () => {
+
   });
 
   it('Should FIND a User by ID', () =>
@@ -67,39 +63,32 @@ describe('MongoDB User CRUD', () => {
         lname,
       },
       { new: true },
-    ).then((doc) => {
-
-      expect(doc.fname).toEqual(fname);
-    });
+    ).then(doc => expect(doc.fname).toEqual(fname));
   });
 
   it('Should REMOVE a User', () =>
     User.findByIdAndRemove(user1ID.toString()).then((doc) => {
-
       expect(doc).toNotBe(null);
     }));
 });
 
 describe('MongoDB Post (Buzz) CRUD', () => {
-  it(`Should CREATE a Post: by User: ${user2ID}`, () => {
+  it(`Should CREATE a Post: by User: ${user2ID}`, () =>
     //  find a user and return non secure information
-    return User.findById(user2ID.toString()).then((user) => {
+    User.findById(user2ID.toString()).then((user) => {
       const newPost = createPost(user.id, postID);// create a new post with the user
-      return newPost.save().then((post) => {
-        return Post.findById(post.id)
+      return newPost.save().then(post =>
+         Post.findById(post.id)
           .populate('owner', 'email fname lname ')
           .then((updatedPost) => {
             user.posts.push(updatedPost.id);
             return user.save().then((updatedDoc) => {
-
               expect(user).toNotBe(null); //  make sure user exists
               expect(updatedPost.owner.id).toEqual(user2ID);
               expect(updatedPost.owner.id).toEqual(updatedDoc.id);
             });
-          });
-      });
-    });
-  });
+          }));
+    }));
 
   it('Should UPDATE a Post', () => {
     const title = 'This is a new title';
@@ -107,8 +96,7 @@ describe('MongoDB Post (Buzz) CRUD', () => {
     return Post.findByIdAndUpdate(postID, {
       title,
       description,
-    }, {new: true}).then((updatedPost) => {
-
+    }, { new: true }).then((updatedPost) => {
       expect(updatedPost.title === title);
     });
   });
@@ -117,9 +105,9 @@ describe('MongoDB Post (Buzz) CRUD', () => {
     Post.findByIdAndRemove(postID)
       .populate('owner')
       .then((deletedPost) => {
-        deletedPost.owner.posts = deletedPost.owner.posts.filter(post => deletedPost.id !== post.toString())
-        return deletedPost.owner.save().then((user) => {
-
+        const owner = deletedPost.owner;
+        owner.posts = owner.posts.filter(post => deletedPost.id !== post.toString());
+        return owner.save().then((user) => {
           expect(deletedPost).toNotBe(null);
           expect(user.posts.indexOf(deletedPost.id) >= 0).toBe(false);
         });
